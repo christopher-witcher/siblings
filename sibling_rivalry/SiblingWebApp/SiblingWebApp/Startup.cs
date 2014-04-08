a4e2b460b6d006989da5602eac0ef6188c81f366
christopher-witcher <christopher.witcher@gmail.com>
2014-04-08 05:26:36 -0700

    Initial Site Commit

---
+++ sibling_rivalry/SiblingWebApp/SiblingWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiblingWebApp.Startup))]
+namespace SiblingWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
